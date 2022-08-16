080a9d3e611fa517098809c6932f8010ef8f34b1
qobiljon <qobiljon.toshnazarov@gmail.com>
2022-08-16 12:04:30 +0900

    download working on python3.6

--- ET_Dashboard/settings.py
+++ ET_Dashboard/settings.py
@@ -112,14 +112,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-# CSRF_COOKIE_DOMAIN = 'etdb1.myvnc.com' # todo enable when deploying
-# CSRF_TRUSTED_ORIGINS = ['etdb1.myvnc.com'] # todo enable when deploying
+CSRF_COOKIE_DOMAIN = 'etdb1.myvnc.com' # todo enable when deploying
+CSRF_TRUSTED_ORIGINS = ['etdb1.myvnc.com'] # todo enable when deploying
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
--- tools/db_mgr.py
+++ tools/db_mgr.py
@@ -316,7 +316,7 @@ def get_filtered_data_records(db_user, db_campaign, db_data_source, from_timesta
 def dump_data(db_campaign, db_user):
     file_path = utils.get_download_file_path(f'cmp{db_campaign.id}_usr{db_user.id}.bin.csv')
     # session.execute(f'copy "data"."cmp{db_campaign.id}_usr{db_user.id}" to %s with header = true;', (file_path,))
-    subprocess.run(['cqlsh', '-e', f"copy data.cmp{db_campaign.id}_usr{db_user.id} to \'{file_path}\' with header = true;"], capture_output=False)
+    subprocess.run(['/root/ET_Dashboard/venv/bin/cqlsh', '-e', f"copy data.cmp{db_campaign.id}_usr{db_user.id} to \'{file_path}\' with header = true;"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     return file_path
 
 
