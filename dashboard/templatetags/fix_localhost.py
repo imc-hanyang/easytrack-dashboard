94a4c7e01f4ec767d1a28d3e1281630b8286a1b7
Qobiljon <qobiljon.toshnazarov@gmail.com>
2023-02-09 19:50:46 +0900

    works with current libarry

--- dashboard/templatetags/fix_localhost.py
+++ dashboard/templatetags/fix_localhost.py
@@ -5,4 +5,4 @@ register = template.Library()
 
 @register.filter
 def fix_localhost(value: str):
-	return value.replace('host.docker.internal', 'localhost').replace('172.17.0.1', 'localhost')
+  return value.replace('host.docker.internal', 'localhost').replace('172.17.0.1', 'localhost')
